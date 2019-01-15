5c25930a971fcb44a9b93ee32f2835efa5a2bc5c
footios <footios76@gmail.com>
2019-01-15 11:34:02 +0200

    using every

--- README.md
+++ README.md
@@ -57,3 +57,7 @@ So we are finished with the relative positioning and we may go on with our cours
 ### commit (in simpler solution branch): simpler solution added
 
 We found a simpler solution in Q&A and we added it to the project in the according branch.
+
+### commit: using every
+
+In this case, we paused the video, and we tried to find a solution, for if there are no ingredients selected. So we used the every method, instead of the reduce.
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,18 +4,24 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 // child of BurgerBuilder
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    // transform the igKey, string value, into an array
-    // with as many elements as the value of the igKey
-    // We use an underscore in the second map func,
-    // because we don't care about the element itself,
-    // since we are going to pass it to the <BurgerIngredient />
-    // through 'type'.
-    return [...Array(props.ingredients[igKey])].map((_, index) => (
-      <BurgerIngredient key={igKey + index} type={igKey} />
-    ));
-  });
-  console.log(transformedIngredients);
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      // transform the igKey, string value, into an array
+      // with as many elements as the value of the igKey
+      // We use an underscore in the second map func,
+      // because we don't care about the element itself,
+      // since we are going to pass it to the <BurgerIngredient />
+      // through 'type'.
+      return [...Array(props.ingredients[igKey])].map((_, index) => (
+        <BurgerIngredient key={igKey + index} type={igKey} />
+      ));
+    })
+    .every(ing => ing.length === 0);
+
+  if (transformedIngredients) {
+    transformedIngredients = <p> Please start adding ingredients! </p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
