b9d2f4f8ee05844c953559eb7ffce59a915ca2a4
footios <footios76@gmail.com>
2019-01-21 09:38:45 +0200

    added css
    We also added the component Menu

---
+++ src/components/Navigation/Toolbar/Menu/Menu.js
@@ -0,0 +1,9 @@
+import React from "react";
+import classes from "./Menu.module.css";
+
+// child of Toolbar
+const Menu = props => {
+  return <div className={classes.Menu}>MENU</div>;
+};
+
+export default Menu;
